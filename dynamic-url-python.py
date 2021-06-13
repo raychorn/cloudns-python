0572f4d348660bf5eea60c626ac73b09c1c9bc0a
Ray C Horn <raychorn@gmail.com>
2021-06-13 09:30:05 -0600

    updates. added some output while the  code runs forever.

--- dynamic-url-python.py
+++ dynamic-url-python.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import os
 import sys
 import json
 
@@ -23,13 +23,14 @@ while (1):
         _origin = data.get("origin")
         assert isinstance(_origin, str) and (len(_origin) > 0), 'Something went wrong. Please fix.'
         print("The public IP is : %s" % _origin)
-        __cache__[_origin] = __cache__.get('ip', 0) + 1
+        __cache__[_origin] = __cache__.get(_origin, 0) + 1
         num_found = len(list(__cache__.keys()))
         detections = [(v > 2) for v in list(__cache__.values())]
         test1 = (num_found > 1)
         test2 = all(detections)
         prediction = (test1) and (test2)
         print('DEBUG: Number of IPs found: {} ({}) --> {} ({}) -> [{}]'.format(num_found, test1, detections, test2, prediction))
+        print('DEBUG: {}'.format(json.dumps(__cache__)))
         if (prediction):
             print('The ip address has changed.')
             DYNAMIC_DNS_URL = "https://ipv4.cloudns.net/api/dynamicURL/?q=MzM1MTk3OToyMzYzMjgzNDI6ZWQwMGM2N2NmYWMyNWE5MzU5ZDI3MGRjODA5OTcwZTk4OTI5YTQwZjhkZWU5YmZiN2IxMWE5OGY0ZjIwMTBmNw"
@@ -42,5 +43,19 @@ while (1):
                 page = urllib.request.urlopen(DYNAMIC_DNS_URL);
                 page.close();    
             __cache__ = {}
-        print('Completed one check.')
+        the_sum = sum(v for v in list(__cache__.values()))
+        fname = os.sep.join([os.path.dirname(__file__), 'results.json'])
+        with open(fname, 'w') as fOut:
+            the_results = {}
+            the_results['__cache__'] = __cache__
+            the_results_diagnostics = the_results.get('diagnostics', {})
+            the_results_diagnostics['num_found'] = num_found
+            the_results_diagnostics['detections'] = detections
+            the_results_diagnostics['test1'] = test1
+            the_results_diagnostics['test2'] = test2
+            the_results_diagnostics['prediction'] = prediction
+            the_results_diagnostics['the_sum'] = the_sum
+            the_results['diagnostics'] = the_results_diagnostics
+            print(json.dumps(the_results, indent=3), file=fOut)
+        print('Completed {} check{}.'.format(the_sum, 's' if (the_sum > 1) else ''))
         time.sleep(10)
