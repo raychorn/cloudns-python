b607f4a8a2536340d2693f530007cdcccb3708c5
Ray C Horn <raychorn@gmail.com>
2021-09-05 15:12:39 +0000

    updates.

--- runner.sh
+++ runner.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DIR0=$(dirname $0)
+#echo "DIR0:$DIR0"
+
 PY=$(which python3.9)
 
 if [[ ! -f "$PY" ]]; then
@@ -7,9 +10,27 @@ if [[ ! -f "$PY" ]]; then
     exit 1
 fi
 
-if [[ ! -f "$(pwd)/dynamic-url-python.py" ]]; then
-    echo "Cannot find $(pwd)/dynamic-url-python.py. Cannot continue."
+if [[ ! -f "$DIR0/dynamic-url-python.py" ]]; then
+    echo "Cannot find $DIR0/dynamic-url-python.py. Cannot continue."
     exit 1
 fi
 
-$PY $(pwd)/dynamic-url-python.py
+SEMAPHORE=/tmp/semaphore_dynamic-url-python
+
+if [[ ! -f "$SEMAPHORE" ]]; then
+    echo "RUNNING" > $SEMAPHORE
+else
+    echo "Already running."
+    exit
+fi
+
+while :
+do
+    $PY $DIR0/dynamic-url-python.py
+    echo "Sleeping..."
+	sleep 10s
+done
+
+if [[ -f "$SEMAPHORE" ]]; then
+    rm -f $SEMAPHORE
+fi
